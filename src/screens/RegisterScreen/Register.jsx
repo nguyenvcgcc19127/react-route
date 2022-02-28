27e6f6fc62c3e19779c9d813cee7dd70e7bb30c3
Nguyen Vo <nguyenvcgcc19127Gfpt.edu.vn>
2022-02-28 11:55:15 +0700

    Updating registration function

--- src/screens/RegisterScreen/Register.jsx
+++ src/screens/RegisterScreen/Register.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import './register.css';
 import { useState } from "react";
+import isEmail from "validator/lib/isEmail";
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
     // States for registration
+    const navigate = useNavigate();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
@@ -11,9 +14,8 @@ export default function Register() {
  
     // States for checking the errors
     const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
-    const [errMessage, setErrMessage] = useState("")
-    const [successMessage, setSuccessMessage] = useState("Ban da dang ki thanh cong")
+    const [errMessage, setErrMessage] = useState({})
+    // const [successMessage, setSuccessMessage] = useState("Ban da dang ki thanh cong")
     
     // Handling the name change
     const handleName = (e) => {
@@ -42,78 +44,89 @@ export default function Register() {
     // Handling the form submission
     const handleSubmit = (e) => {
         e.preventDefault();
+        const msg = {}
         console.log(name, email, phone, password)
-        if (name === '' || email === '' || password === '' || phone === '') {
-            setError(true);
-            setErrMessage("Chua nhap thong tin day du")
-        }
-        else if(password.length < 8 || password.length > 16){
-            setError(true);
-            setErrMessage("Giới hạn mật khẩu là từ 8 đến 16")
-        }
-        else if (phone.length < 10 || phone.length > 11){
-            setError(true);
-            setErrMessage("Số điện thoại phải là 10 số")
-        }else {
-            setSubmitted(true);
+        if(name === '' || email === '' || phone === '' || password === ''){
+            if (name === '') {
+                msg.name = "Vui lòng nhập họ và tên!"
+            } if (email === '') {
+                msg.email = "Vui lòng nhập email!"
+            } else if (!isEmail(email)) {
+                msg.email = "Email không đúng!"
+            } if (password === '') {
+                msg.password = "Vui lòng nhập mật khẩu!"
+                
+            } else if(password.length < 8 || password.length > 16){
+                msg.password = "Giới hạn mật khẩu là từ 8 đến 16 ký tự!"
+            } if (phone === '') {
+                msg.phone = "Vui lòng nhập số điện thoại!"
+                
+            } else if (phone.length < 10 || phone.length > 11){
+                msg.phone = "Số điện thoại phải là 10 số!"
+            } 
+        } else {
+            setSubmitted(true)
+            navigate('/');
             console.log(e);
-        }  
+        } 
+        
+        setErrMessage(msg)
+        if (Object.keys(msg).length > 0) return false
+        else return true
     };
     
-    console.log(error)
     return (
-        <div className="container">
-            <div className="text">
-                <h1>User Registration</h1>
-            </div>
-            <div className="text">
-                { 
-                    error === true ? errMessage :<span style={{color: "green"}}>{successMessage}</span> 
-                }  
-            </div>
-            <div className="row">
-                <form onSubmit={handleSubmit}>
-                    <div className="col-lg-3">
-                        <label>Họ và tên:
-                            <input onChange={handleName}
+        <div className="container-item">
+            <div className="app-wrapper">
+                <form className="form-wrapper" onSubmit={handleSubmit}>
+                    <div className="text title">
+                        <h1>Đăng ký</h1>
+                    </div>
+                    <div className="name">
+                            <input className="input" onChange={handleName}
                             type="text"
+                            name="name"
+                            id="name"
                             value={name}
                             placeholder="Họ và tên" />
-                        </label>
+                        <p className="p1" style={{color: "red"}}>{ errMessage.name }</p>
                     </div>
 
-                    <div className="col-lg-3">
-                        <label>Số điện thoại:
-                            <input onChange={handlePhone}
+                    <div className="phone">
+                            <input className="input" onChange={handlePhone}
+                            name="phone"
+                            id="phone"
                             value={phone}
                             placeholder="Số điện thoại" />
-                        </label>
+                        <p className="p1" style={{color: "red"}}>{ errMessage.phone }</p>
                     </div>
 
-                    <div className="col-lg-3">
-                        <label>Email:
-                        </label>
-                        <input onChange={handleEmail}
-                            type='email' 
+                    <div className="email">
+                        <input className="input" onChange={handleEmail} 
+                            name="email"
+                            id="email"
+                            autoComplete="email"
                             value={email}
-                            placeholder="Email" />
+                            placeholder="email@gmail.com" />
+                        <p className="p1" style={{color: "red"}}>{ errMessage.email }</p>
                     </div>
                     
-                    <div className="col-lg-3">
-                        <label>Mật khẩu:
-                            <input onChange={handlePassword}
+                    <div className="password">
+                            <input className="input" onChange={handlePassword}
+                            name="password"
+                            id="password"
                             value={password}
                             type="password" 
                             placeholder="Mật khẩu" />
-                        </label>
-
+                        <p className="p1" style={{color: "red"}}> { errMessage.password } </p>
                     </div>
 
-                    <button onSubmit={handleSubmit} className="btn btn-success" type="submit">
-                        Submit
-                    </button>
+                    <button onSubmit={handleSubmit} className="submit">Đăng ký</button>
+                    <p className="text text-item">
+                        Bạn đã có tài khoản? <a href="#">Đăng nhập</a>
+                    </p>
                 </form>
-            </div>
+            </div>     
         </div>
     );
 }
